68d66ea5e94f6f4ee407cf6d54ec13bd39043a4d
Durjoy Barua <durjoybarua8115@example.com>
2024-04-19 21:58:11 +0600

    p

--- JavaScript Basic/1.js
+++ JavaScript Basic/1.js
@@ -1,6 +1,24 @@
-            alert("Hello everyone")
-            document.write("I love bangladesh <br/>")
-            document.write("My name is Durjoy Barua<br/>")
-            //console.log("I love japan")
-             var name ="I'm a cse student<br/>"
-            document.write(name);
+            // alert("Hello everyone")
+            // document.write("I love bangladesh <br/>")
+            // document.write("My name is Durjoy Barua<br/>")
+            // //console.log("I love japan")
+            //  var names ="I'm a cse student<br/>"
+            // document.write(names)
+            // var str= "1256"
+            // document.write(parseInt(str)+"<br>")
+            
+            // document.write(Number("          12<br>"))
+            // document.write(Number(true)+"<br>")
+
+          //// new class
+        //   var nam="Durjoy" 
+        //   var last="Barua";
+        //   document.write(nam+" "+last+"<br>")
+        //    var text= prompt("Enter your name: ")
+        //    document.write("Length of "+ text+" is "+ text.length)
+
+        var first= prompt("Enter your first name :");
+        var last= prompt("Enter your last name :");
+        document.write("My name is "+first+" "+last)
+        document.write("<br> Length of your name : "+ (first.concat(last)).length)
+
