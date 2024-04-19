acec2d534454d94ec22da05c266f0c9bb069b7ee
Durjoy Barua <durjoybarua8115@example.com>
2024-04-19 20:37:27 +0600

    push

---
+++ JavaScript Basic/1.js
@@ -0,0 +1,6 @@
+            alert("Hello everyone")
+            document.write("I love bangladesh <br/>")
+            document.write("My name is Durjoy Barua<br/>")
+            //console.log("I love japan")
+             var name ="I'm a cse student<br/>"
+            document.write(name);
